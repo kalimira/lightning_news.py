258f3a2b5ae778fdec506c164cfcb685f28adbef
kalimira <124667243+kalimira@users.noreply.github.com>
2025-12-17 20:46:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -268,5 +268,7 @@ def full_article(article_id):
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 8081))
+    app.run(host="0.0.0.0", port=port)
 
